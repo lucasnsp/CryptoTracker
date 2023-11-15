84dc54774f26d9d5ac8a51977bc36eb49c30c06d
Lucas Neves <lucas.neves12@icloud.com>
2023-11-15 17:47:13 -0300

    Creating extension for columnTitles.

--- CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
@@ -22,6 +22,8 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
+                columnTitles
+                
                 if !showPortfolio {
                    allCoinsList
                         .transition(.move(edge: .leading))
@@ -88,5 +90,20 @@ extension HomeView {
             }
         }
         .listStyle(PlainListStyle())
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundStyle(Color.theme.secondaryText)
+        .padding(.horizontal)
     }
 }
