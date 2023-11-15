e9874657a556b41d3501a5090bf0e1f2e1a887c3
Lucas Neves <lucas.neves12@icloud.com>
2023-11-15 15:07:09 -0300

    Created PreviewProvider extension for or CoinRowView be more realistc it possible and also be able to use in other views.

---
+++ CryptoTracker/CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,19 @@
+//
+//  CoinRowView.swift
+//  CryptoTracker
+//
+//  Created by Lucas Neves dos santos pompeu on 15/11/23.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
