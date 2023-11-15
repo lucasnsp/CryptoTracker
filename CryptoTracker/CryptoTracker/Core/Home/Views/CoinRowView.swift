d728ff6e8033e13d0fce62321101e3809c00d51c
Lucas Neves <lucas.neves12@icloud.com>
2023-11-15 15:23:17 -0300

    Creating the Rows for the coin View.

--- CryptoTracker/CryptoTracker/Core/Home/Views/CoinRowView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -12,7 +12,30 @@ struct CoinRowView: View {
     let coin: CoinModel
     
     var body: some View {
-        Text(coin.name)
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundStyle(Color.theme.accent)
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("\(coin.currentPrice)")
+                    .bold()
+                    .foregroundStyle(Color.theme.accent)
+                Text("\(coin.priceChangePercentage24H ?? 0)%")
+                    .foregroundStyle(
+                        (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                        Color.theme.green :
+                        Color.theme.red
+                    )
+            }
+        }
     }
 }
 
