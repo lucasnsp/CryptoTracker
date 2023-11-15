f6b2bc7bed364905783539ee9981fbced6e336e0
Lucas Neves <lucas.neves12@icloud.com>
2023-11-15 14:43:37 -0300

    Creating computed variable for the currentHoldingsValue.

--- CryptoTracker/CryptoTracker/Models/CoinModel.swift
+++ CryptoTracker/CryptoTracker/Models/CoinModel.swift
@@ -101,6 +101,10 @@ struct CoinModel: Identifiable, Codable {
     func updateHoldins(amount: Double) -> CoinModel {
         return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amount)
     }
+    
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
 }
 
 struct SparklineIn7D: Codable {
