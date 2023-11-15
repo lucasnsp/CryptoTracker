40bb2cbca2152f6ddd13a81cc84d51da2e9a93bc
Lucas Neves <lucas.neves12@icloud.com>
2023-11-15 15:39:27 -0300

    Creating a extension to double to converts a double into a currency that will only be able to have a minimum of 2 decimal places and a max of 6.

---
+++ CryptoTracker/CryptoTracker/Extensions/Double.swift
@@ -0,0 +1,39 @@
+//
+//  Double.swift
+//  CryptoTracker
+//
+//  Created by Lucas Neves dos santos pompeu on 15/11/23.
+//
+
+import Foundation
+
+extension Double {
+    
+    /// Converts a Double into a Currency with 2-6 decimal places
+    /// ```
+    ///  Convert 1234.56 to $1,234.56
+    ///  Convert 12.3456 to $12.3456
+    ///  Convert 0.123456 to $0.123456
+    /// ```
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+//        formatter.locale = .current // <- default value
+//        formatter.currencyCode = "usd" // <- change currency
+//        formatter.currencySymbol = "$" // <- change currency symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    /// Converts a Double into a Currency as a string with 2-6 decimal places
+    /// ```
+    ///  Convert 1234.56 to "$1,234.56"
+    ///  Convert 12.3456 to "$12.3456"
+    ///  Convert 0.123456 to "$0.123456"
+    /// ```
+    public func asCurrencyWith6Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter6.string(from: number) ?? "0.00"
+    }
+}
