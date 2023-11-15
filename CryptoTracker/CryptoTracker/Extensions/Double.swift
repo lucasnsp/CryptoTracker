2c4289bbbf79561cc8a1f508b6b842c2328025f7
Lucas Neves <lucas.neves12@icloud.com>
2023-11-15 16:36:06 -0300

    Created the currencyFormatter2 that have an maximum of 2 fractionDigits.

--- CryptoTracker/CryptoTracker/Extensions/Double.swift
+++ CryptoTracker/CryptoTracker/Extensions/Double.swift
@@ -9,6 +9,31 @@ import Foundation
 
 extension Double {
     
+    /// Converts a Double into a Currency with 2 decimal places
+    /// ```
+    ///  Convert 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.usesGroupingSeparator = true
+        formatter.locale = .autoupdatingCurrent
+        formatter.currencyCode = "usd" // <- change currency
+        formatter.currencySymbol = "$" // <- change currency symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converts a Double into a Currency as a string with 2-6 decimal places
+    /// ```
+    ///  Convert 1234.56 to "$1,234.56"
+    /// ```
+    public func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
     /// Converts a Double into a Currency with 2-6 decimal places
     /// ```
     ///  Convert 1234.56 to $1,234.56
