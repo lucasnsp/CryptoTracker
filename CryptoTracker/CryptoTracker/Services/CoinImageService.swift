24432fdb023bce9e0c9ee61236efbe07a25c6cca
Lucas Neves <lucas.neves12@icloud.com>
2023-11-16 11:38:14 -0300

    Created CoinImageService

---
+++ CryptoTracker/CryptoTracker/Services/CoinImageService.swift
@@ -0,0 +1,35 @@
+//
+//  CoinImageService.swift
+//  CryptoTracker
+//
+//  Created by Lucas Neves dos santos pompeu on 16/11/23.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    var imageSubscription: AnyCancellable?
+    
+    init(urlString: String) {
+        getCoinImage(urlString: urlString)
+    }
+    
+    private func getCoinImage(urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+    
+}
