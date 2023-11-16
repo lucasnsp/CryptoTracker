04ba5a9f55959c92a136d98a4d975f738f026f5d
Lucas Neves <lucas.neves12@icloud.com>
2023-11-16 17:31:44 -0300

    Creating class for fileManager and function to saveImage.

---
+++ CryptoTracker/CryptoTracker/Utilities/LocalFileManager.swift
@@ -0,0 +1,30 @@
+//
+//  LocalFileManager.swift
+//  CryptoTracker
+//
+//  Created by Lucas Neves dos santos pompeu on 16/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    private init() { }
+    
+    func saveImage(image: UIImage) {
+        
+        guard 
+            let data = image.pngData(),
+            let url = URL(string: "")
+            else { return }
+        
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("Error saving image. \(error)")
+        }
+    }
+    
+}
