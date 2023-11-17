c6d3ecfdcaa0b0c48cf9a3909a195552e7053fe7
Lucas Neves <lucas.neves12@icloud.com>
2023-11-17 19:30:45 -0300

    SearchBar UI created and customized.

---
+++ CryptoTracker/CryptoTracker/Core/Components/SearchBar/SearchBarView.swift
@@ -0,0 +1,47 @@
+//
+//  SearchBarView.swift
+//  CryptoTracker
+//
+//  Created by Lucas Neves dos santos pompeu on 17/11/23.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @State var searchText: String = ""
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+            
+            TextField("Search by name or symbol...", text: $searchText)
+                .foregroundStyle(Color.theme.accent)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundStyle(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                    , alignment: .trailing
+                    
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.accent.opacity(0.15),
+                        radius: 10, x: 0, y: 0)
+        )
+        .padding()
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
