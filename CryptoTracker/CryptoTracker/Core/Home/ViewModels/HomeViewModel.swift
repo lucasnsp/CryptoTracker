123faf01faab87a03da5746e6e0a0c780916e7b4
Lucas Neves <lucas.neves12@icloud.com>
2023-11-17 20:19:33 -0300

    Updating function (addSubscribers ) to react to changes in the search text and available coins, filtering and updating the list of all coins in HomeViewModel based on the search performed.

--- CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,7 +23,24 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) -> [CoinModel] in
+                
+                guard !text.isEmpty else {
+                    return startingCoins
+                }
+                
+                let lowercasedText = text.lowercased()
+                
+                return startingCoins.filter { coin in
+                    return  coin.name.lowercased().contains(lowercasedText) ||
+                            coin.symbol.lowercased().contains(lowercasedText) ||
+                            coin.id.lowercased().contains(lowercasedText)
+                }
+            }
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
