0b0e0b2100c5282cb86f478f45321d99585b1f52
Lucas Neves <lucas.neves12@icloud.com>
2023-11-17 21:47:10 -0300

    Created the view for the StatisticView and the logic for the data in the View.

--- CryptoTracker/CryptoTracker/Core/Components/Statistic/StatisticView.swift
+++ CryptoTracker/CryptoTracker/Core/Components/Statistic/StatisticView.swift
@@ -12,10 +12,29 @@ struct StatisticView: View {
     let stat: StatisticModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 4) {
+            Text(stat.title)
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+            Text(stat.value)
+                .font(.headline)
+                .foregroundStyle(Color.theme.accent)
+            HStack(spacing: 4) {
+                Image(systemName: "triangle.fill")
+                    .font(.caption2)
+                    .rotationEffect(
+                        Angle(degrees: (stat.percentageChange ?? 0) >= 0 ? 0 : 180))
+                
+                Text(stat.percentageChange?.asPercentageString() ?? "")
+                    .font(.caption)
+                    .bold()
+            }
+            .foregroundStyle((stat.percentageChange ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+            .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
+        }
     }
 }
 
 #Preview {
-    StatisticView()
+    StatisticView(stat: DeveloperPreview.instance.stat1)
 }
--- CryptoTracker/CryptoTracker/Extensions/PreviewProvider.swift
+++ CryptoTracker/CryptoTracker/Extensions/PreviewProvider.swift
@@ -22,8 +22,8 @@ class DeveloperPreview {
     
     let homeVM = HomeViewModel()
     
-    let state1 = StatisticModel(title: "Market Cap", value: "$12.5Bn", percentageChange: 25.34)
-    let state2 = StatisticModel(title: "Total Volume", value: "$1.23Tr")
+    let stat1 = StatisticModel(title: "Market Cap", value: "$12.5Bn", percentageChange: 25.34)
+    let stat2 = StatisticModel(title: "Total Volume", value: "$1.23Tr")
     
     let coin = CoinModel(
        id: "bitcoin",
