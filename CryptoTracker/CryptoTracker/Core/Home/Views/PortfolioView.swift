27393c85b0279dccee415d22eb317def113939b8
Lucas Neves <lucas.neves12@icloud.com>
2023-11-18 15:30:12 -0300

    Creating the UI for the user put the data of his portfolio.

--- CryptoTracker/CryptoTracker/Core/Home/Views/PortfolioView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -11,14 +11,38 @@ struct PortfolioView: View {
     
     @EnvironmentObject private var vm: HomeViewModel
     @State private var selectedCoin: CoinModel? = nil
+    @State private var quantitytext: String = ""
     
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
+                    coinLogoList
                     
-                   coinLogoList
+                    if selectedCoin != nil {
+                        VStack(spacing: 20) {
+                            HStack {
+                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                                Spacer()
+                                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+                            }
+                            Divider()
+                            HStack {
+                                Text("Amount in your portfolio:")
+                                Spacer()
+                                TextField("Ex: 1.4", text: $quantitytext)
+                                    .multilineTextAlignment(.trailing)
+                                    .keyboardType(.decimalPad)
+                            }
+                            Divider()
+                            HStack {
+                                Text("Current value:")
+                                Spacer()
+                                Text("")
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -60,5 +84,9 @@ extension PortfolioView {
             .padding(.vertical, 4)
             .padding(.leading)
         }
+    }
+    
+    private func getCurrentValue() -> Double {
+        
     }
 }
