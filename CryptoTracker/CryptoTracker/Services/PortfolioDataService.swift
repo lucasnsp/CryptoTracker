a5b02ed982b6a44c04e294ce9f13205a02ee9586
Lucas Neves <lucas.neves12@icloud.com>
2023-11-19 20:06:58 -0300

    Created portfolio container and file of the PortfolioDataService.

---
+++ CryptoTracker/CryptoTracker/Services/PortfolioDataService.swift
@@ -0,0 +1,12 @@
+//
+//  PortfolioDataService.swift
+//  CryptoTracker
+//
+//  Created by Lucas Neves dos santos pompeu on 19/11/23.
+//
+
+import Foundation
+
+class PortfolioDataService {
+    
+}
