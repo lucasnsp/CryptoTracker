5cea3c8a4a4de619873e05cd53b4346d74f52fa9
Lucas Neves <lucas.neves12@icloud.com>
2023-11-19 22:35:09 -0300

    Created function to updated the selectedCoin in the PortfolioView and now the portfolioView first shows the coins that the user haver in the portfolio and when he clicks on the searchBar and types something load the other coins.

--- CryptoTracker/CryptoTracker/Core/Home/Views/PortfolioView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -55,13 +55,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal) {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background(
@@ -72,6 +72,17 @@ extension PortfolioView {
             }
             .frame(height: 120)
             .padding(.leading)
+        }
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == coin.id }),
+           let amount = portfolioCoin.currentHoldings {
+            quantitytext = "\(amount)"
+        } else {
+            quantitytext = ""
         }
     }
     
@@ -126,9 +137,13 @@ extension PortfolioView {
     
     private func saveButtonPressed() {
         
-        guard let coin =  selectedCoin else { return }
+        guard
+            let coin = selectedCoin,
+            let amount = Double(quantitytext)
+            else { return }
         
         // save to portfolio
+        vm.updatePortfolio(coin: coin, amount: amount)
         
         // show checkmark
         withAnimation(.easeIn) {
